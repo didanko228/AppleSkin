3aa2170aabc229d0a0f85c520bcda6a036585e15
”didanko228” <51835948+didanko228@users.noreply.github.com>
2026-07-17 12:18:39 +0300

    26.3-snapshot-4

--- java/squeek/appleskin/helpers/KeyHelper.java
+++ java/squeek/appleskin/helpers/KeyHelper.java
@@ -1,27 +1,23 @@
 package squeek.appleskin.helpers;
 
 import com.mojang.blaze3d.platform.InputConstants;
-import com.mojang.blaze3d.platform.Window;
-import net.minecraft.client.Minecraft;
 import net.minecraft.util.Util;
-import org.lwjgl.glfw.GLFW;
+import org.lwjgl.sdl.SDLScancode;
 
 public class KeyHelper
 {
 	public static boolean isCtrlKeyDown()
 	{
-		Window window = Minecraft.getInstance().getWindow();
 		// prioritize CONTROL, but allow OPTION as well on Mac (note: GuiScreen's isCtrlKeyDown only checks for the OPTION key on Mac)
-		boolean isCtrlKeyDown = InputConstants.isKeyDown(window, GLFW.GLFW_KEY_LEFT_CONTROL) || InputConstants.isKeyDown(window, GLFW.GLFW_KEY_RIGHT_CONTROL);
+		boolean isCtrlKeyDown = InputConstants.isKeyDown(SDLScancode.SDL_SCANCODE_LCTRL) || InputConstants.isKeyDown(SDLScancode.SDL_SCANCODE_RCTRL);
 		if (!isCtrlKeyDown && Util.getPlatform() == Util.OS.OSX)
-			isCtrlKeyDown = InputConstants.isKeyDown(window, GLFW.GLFW_KEY_LEFT_SUPER) || InputConstants.isKeyDown(window, GLFW.GLFW_KEY_RIGHT_SUPER);
+			isCtrlKeyDown = InputConstants.isKeyDown(SDLScancode.SDL_SCANCODE_LGUI) || InputConstants.isKeyDown(SDLScancode.SDL_SCANCODE_RGUI);
 
 		return isCtrlKeyDown;
 	}
 
 	public static boolean isShiftKeyDown()
 	{
-		Window window = Minecraft.getInstance().getWindow();
-		return InputConstants.isKeyDown(window, GLFW.GLFW_KEY_LEFT_SHIFT) || InputConstants.isKeyDown(window, GLFW.GLFW_KEY_RIGHT_SHIFT);
+		return InputConstants.isKeyDown(SDLScancode.SDL_SCANCODE_LSHIFT) || InputConstants.isKeyDown(SDLScancode.SDL_SCANCODE_RSHIFT);
 	}
 }
